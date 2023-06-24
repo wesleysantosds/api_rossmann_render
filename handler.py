87dbd7944ddfe43a91b3c0541f2bd0146dd87874
Wesley Santos <wesleysantos1995@outlook.com>
2023-06-24 19:27:18 -0300

    initial commit

--- handler.py
+++ handler.py
@@ -43,5 +43,5 @@ def rossmann_predict():
         return Reponse( '{}', status=200, mimetype='application/json' )
 
 if __name__ == '__main__':
-    port = os.environ( 'PORT', 5000)
+    port = os.environ.get( 'PORT', 5000)
     app.run( host='0.0.0.0', port=port )
